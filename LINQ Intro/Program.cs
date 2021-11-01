864fca5366405f9c17d4ed1dc5a6ed4016b3e7e4
nicomp42 <nicholdw@ucmail.uc.edu>
2021-11-01 12:33:23 -0400

    Add project files.

---
+++ LINQ Intro/Program.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace LINQ_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo04();
+        }
+        /// <summary>
+        /// Demonstrate using LINQ to query an array of Strings
+        /// </summary>
+        public static void Demo01()
+        {
+            // Data source
+            string[] teams = { "Bengals", "Steelers", "Ravens", "Browns" };
+
+            // LINQ Query , note the data type
+            IEnumerable<string> myLINQQuery = from team in teams
+                                              orderby team
+                                              select team;
+
+            // Query execution
+            foreach (String team in myLINQQuery)
+            {
+                Console.Write(team + " ");
+            }
+        }
+        /// <summary>
+        /// Demonstrate using LINQ to query a text file. Spoiler alert: we read it into an array.
+        /// </summary>
+        public static void Demo02()
+        {
+            // Data source is a text file
+            //int[][] data = File.ReadAllLines("..\\..\\data.txt").Select(l => l.Split(' ').Select(int.Parse).ToArray()).ToArray();
+            int[][] data = File.ReadAllLines(@"..\..\data.txt").Select(l => l.Split(' ').Select(int.Parse).ToArray()).ToArray();
+
+            // LINQ Query, note that the query is parsed at compile time. This is a good time to use the 'var' data type, then hover !
+            var myLINQQuery = from row in data
+                              select row;
+
+            // Query execution
+            foreach (int[] row in myLINQQuery)
+            {
+                Console.WriteLine(row[0] + ", " + row[1] + " ");
+            }
+        }
+        /// <summary>
+        /// Demonstrate using LINQ to query a JSON file.
+        /// Adapted from https://markheath.net/post/linq-to-general-election-part-1-linq-to 
+        /// </summary>
+        public static void Demo03()
+        {
+            // This JSON download has a schema we can investigate
+            string url = "https://tools.learningcontainer.com/sample-json.json";
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString(url);
+            JObject jo = JObject.Parse(json);         // Hover over JObject
+            Console.WriteLine("JSON data...");
+            Console.WriteLine(jo["firstName"].ToString());  // Case-sensitive!
+            Console.WriteLine(jo["unknownkey"]);            // This fails quietly
+        }
+        /// <summary>
+        /// Use an API key to access data from openweathermap.org
+        /// </summary>
+        public static void Demo04()
+        {
+            String apiKey = @"407d3c698b542dff9b83708c98c93e30";
+            string bataviaOHLatLong = "39.0770° N, 84.1769° W";
+            // This JSON download has a schema we can investigate
+            string currentWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=39.0770&lon=84.1769&exclude=&appid=" + apiKey;
+            string url = "http://bulk.openweathermap.org/snapshot/weather_14.json.gz?appid=" + apiKey;
+            Console.WriteLine(url);
+            Console.WriteLine(currentWeatherURL);
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString(currentWeatherURL);
+            JObject jo = JObject.Parse(json);         // Hover over JObject
+            Console.WriteLine("JSON data...");
+            Console.WriteLine(jo["timezone"].ToString());  // Case-sensitive!
+            Console.WriteLine(jo["current"]["temp"].ToString());
+            Console.WriteLine(jo["current"]["weather"][0]["description"].ToString());
+
+            // Select the structure for each ["hourly"] data point
+            var myLINQQuery = from JoRow in jo["hourly"]
+                              select JoRow;
+
+            // Query execution
+            foreach (var row in myLINQQuery)
+            {
+                //                Console.WriteLine(row);   // This yields everything in each ["hourly"][n] data structure
+                Console.WriteLine(row["temp"]);
+            }
+        }
+        public void Demo05()
+        {
+
+        }
+    }
+}
---
+++ PizzaOrder/PizzaOrder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaOrderNamespace
+{
+    public class PizzaOrder
+    {
+        private List<String> mToppings;
+        private String mCrust;
+        public enum enumPizzaSize { slice, personal, small, medium, large, extraLarge };
+        private enumPizzaSize mPizzaSize;
+
+        public PizzaOrder()
+        {
+
+        }
+
+        public enumPizzaSize pizzaSize {
+            get { return mPizzaSize; }
+            set { mPizzaSize = value; }
+        }
+
+        /// <summary>
+        /// Crust of the pizza
+        /// </summary>
+        public String crust
+        {
+            get { return mCrust; }
+            set { mCrust = value; }
+        }
+        /// <summary>
+        /// Toppings list for this pizza
+        /// </summary>
+        public List<String> toppings
+        {
+            get { return new List<String>(mToppings); }
+            set { mToppings = new List<String>(value); }  
+        }
+        /// <summary>
+        /// Convert current PizzaOrder to a string representation
+        /// </summary>
+        /// <returns></returns>
+        public override String ToString()
+        {
+            return crust + ": " + String.Join(", ", mToppings.ToArray());
+        }
+
+    }
+}
